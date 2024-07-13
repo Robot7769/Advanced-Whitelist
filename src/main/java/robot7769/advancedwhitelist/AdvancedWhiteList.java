32a9430c54789d5332dea3faa8b43822eb90fd91
Robot7769 <jan.robot7@gmail.com>
2024-07-14 00:32:11 +0200

    feat: create Advanced White List plugin

---
+++ src/main/java/robot7769/advancedwhitelist/AdvancedWhiteList.java
@@ -0,0 +1,19 @@
+package robot7769.advancedwhitelist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedWhiteList extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
